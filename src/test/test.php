d2da865d7a836201b98b745dfcbc85f8e739f215
DESKTOP-QP7I888\shaban <shaban@everdreamsoft.com>
2019-02-10 12:30:53 +0100

    just initial tests

---
+++ src/test/test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 10.02.2019
+ * Time: 12:22
+ */
+
+
+require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
+
+use SandraDG\SayHello;
+
+echo SayHello::world();
