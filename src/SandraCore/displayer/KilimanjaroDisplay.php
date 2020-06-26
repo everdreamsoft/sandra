981ed15eb81015663fba00a436e96f96b8eff4cb
shaban shaame <shaban@everdreamsoft.com>
2020-06-26 23:05:23 +0200

    kilimanjaro display

---
+++ src/SandraCore/displayer/KilimanjaroDisplay.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by EverdreamSoft.
+ * User: Shaban Shaame
+ * Date: 2019-07-19
+ * Time: 11:35
+ */
+
+namespace SandraCore\displayer;
+
+
+
+ class KilimanjaroDisplay extends DisplayType
+{
+
+
+    function getDisplay(): array
+    {
+
+        $displayer = $this->displayer ;
+        $returnArray = array();
+
+
+        //Cycle trough all factories
+        foreach($displayer->factoryArray as  $factory) {
+
+
+            foreach ($factory->entityArray as $key => $entity) {
+
+
+                foreach ($displayer->displayReferenceArray as $referenceObject) {
+
+
+                    $refConceptUnid = $referenceObject->idConcept;
+
+                    //we have a concept name matching the dictionary
+                    $refConceptName = $displayer->getDisplayKey($referenceObject);
+
+                    if(!isset( $entity->entityRefs[$refConceptUnid])) continue ;
+
+                    if(!is_object( $entity->entityRefs[$refConceptUnid])) continue ;
+
+                    $returnArray[$key.'kili'][$refConceptName] = $entity->entityRefs[$refConceptUnid]->refValue;
+
+                }
+
+            }
+        }
+
+        return $returnArray ;
+
+
+    }
+
+
+ }
